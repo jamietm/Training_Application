98d6048ef7d4627a348dbbf34a14070716b7dd66
Jamie <Jamie@JM.home>
2015-08-01 20:21:35 +0100

    New Committ

---
+++ src/peaktrainingapplication/PeakTrainingApplication.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peaktrainingapplication;
+
+import peaktrainingapplication.Pages.Index;
+import peaktrainingapplication.Pages.selectTraining;
+
+/**
+ *
+ * @author Jamie
+ */
+public class PeakTrainingApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Index().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
+
